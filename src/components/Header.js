3fd6e0acdde392db64437e01d9a6398d13a459d1
tuankiet107 <thongtuankiet.1999@gmail.com>
2020-05-15 16:47:33 +0700

    Done Fix file Header for Responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,119 +1,115 @@
-import React, { useState } from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import React, { Component } from 'react';
+import { Container, Row, Col} from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown, Dropdown} from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 
-import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    Nav,
-    NavItem,
-    Dropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-    Button
-} from 'reactstrap';
+class Header extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            formSearch: true,
+            slideCart: true
+        }
+    }
 
+    closeFormSearch = () => {
+        this.setState({
+            formSearch: !this.state.formSearch
+        })
+    }
 
-const Header = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
+    render(){
+        return (
+        <div>
+            <Container fluid>
+                <Row>
+                    <Navbar style={{ width: '100rem', padding: '10px 60px'}} collapseOnSelect expand="md" bg="dark" variant="dark">
+                        <Navbar.Brand href="#">ShopStyle</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="m-auto">
+                                <Nav.Item>
+                                    <Nav.Link href="#features">Home</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link href="#pricing">About</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <NavDropdown title="Product" id="collasible-nav-dropdown">
+                                        <Dropdown>
+                                        <Dropdown.Toggle variant="" id="dropdown-basic"> TOP </Dropdown.Toggle>
+                                            <Dropdown.Menu>
+                                                <Dropdown.Item href="#/action-1">Jackets</Dropdown.Item>
+                                                <Dropdown.Item href="#/action-2">Shirts</Dropdown.Item>
+                                                <Dropdown.Item href="#/action-3">Sweaters & Cardigans</Dropdown.Item>
+                                                <Dropdown.Item href="#/action-2">T-Shirts</Dropdown.Item>
+                                            </Dropdown.Menu>
+                                        </Dropdown>
+                                        <Dropdown>
+                                        <Dropdown.Toggle variant="" id="dropdown-basic"> BOTTOM </Dropdown.Toggle>
+                                            <Dropdown.Menu>
+                                                <Dropdown.Item href="#/action-1">Swimwear</Dropdown.Item>
+                                                <Dropdown.Item href="#/action-2">Skirts</Dropdown.Item>
+                                                <Dropdown.Item href="#/action-3">Jeans</Dropdown.Item>
+                                                <Dropdown.Item href="#/action-2">Trousers</Dropdown.Item>
+                                            </Dropdown.Menu>
+                                        </Dropdown>
+                                        <Dropdown>
+                                        <Dropdown.Toggle variant="" id="dropdown-basic"> CLOTHING </Dropdown.Toggle>
+                                            <Dropdown.Menu>
+                                                <Dropdown.Item href="#/action-1">Top Wear</Dropdown.Item>
+                                                <Dropdown.Item href="#/action-2">Party Wear</Dropdown.Item>
+                                                <Dropdown.Item href="#/action-3">Bottom Wear</Dropdown.Item>
+                                                <Dropdown.Item href="#/action-2">Indian Wear</Dropdown.Item>
+                                            </Dropdown.Menu>
+                                        </Dropdown>
+                                        <Dropdown>
+                                        <Dropdown.Toggle variant="" id="dropdown-basic"> ACCESSORIES </Dropdown.Toggle>
+                                            <Dropdown.Menu>
+                                                <Dropdown.Item href="#/action-1">Bags</Dropdown.Item>
+                                                <Dropdown.Item href="#/action-2">Sunglasses</Dropdown.Item>
+                                                <Dropdown.Item href="#/action-3">Fragrances</Dropdown.Item>
+                                                <Dropdown.Item href="#/action-2">Wallets</Dropdown.Item>
+                                            </Dropdown.Menu>
+                                        </Dropdown>
+                                    </NavDropdown>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Button variant="" onClick={this.closeFormSearch}>
+                                        <i class="fas fa-search"></i>
+                                    </Button>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Button variant="">
+                                        <i class="fas fa-shopping-cart"></i>
+                                    </Button>
+                                </Nav.Item>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </Row>
+            </Container>
 
-  const toggle = () => setIsOpen(!isOpen);
-
-//   const [dropdownOpen, setDropdownOpen] = useState(false);
-
-//   const toggleProduct = () => setDropdownOpen(prevState => !prevState);
-
-  return (
-    <Router>
-      <Navbar color="light" light expand="md">
-        <div href="/" className="logo">ShopStyle</div>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="m-auto" navbar>
-                <NavItem>
-                    <Link to="/" className="nav-link"><span className="btn">Home</span></Link>
-                </NavItem>
-                <NavItem>
-                    <Link to="/about" className="nav-link"><span className="btn">About Us</span></Link>
-                </NavItem>
-                
-                <NavItem>
-                    <Link to="" className="nav-link">
-                        <span className="btn">Product</span>
-                        <table className="type-product">
-                            <tr>
-                                <ul>
-                                    <li><h3>TOP</h3></li>
-                                    <li> <a href="#">Jackets</a> </li>
-                                    <li> <a href="#">T-Shirts</a> </li>
-                                    <li> <a href="#">Shirts</a> </li>
-                                    <li> <a href="#">Sweaters & Cardigans</a> </li>
-                                </ul>
-                                <ul>
-                                    <li><h3>BOTTOM</h3></li>
-                                    <li> <a href="#">Jeans</a> </li>
-                                    <li> <a href="#">Trousers </a> </li>
-                                    <li> <a href="#">Swimwear</a> </li>
-                                    <li> <a href="#">Swimwear</a> </li>
-                                </ul>
-                                <ul>
-                                    <li><h3>CLOTHING</h3></li>
-                                    <li> <a href="#">Top Wear</a> </li>
-                                    <li> <a href="#">Party Wear</a> </li>
-                                    <li> <a href="#">Bottom Wear</a> </li>
-                                    <li> <a href="#">Indian Wear</a> </li>
-                                </ul>
-                                <ul>
-                                    <li><h3>ACCESSORIES</h3></li>
-                                    <li> <a href="#">Bags</a> </li>
-                                    <li> <a href="#">Sunglasses</a> </li>
-                                    <li> <a href="#">Fragrances</a> </li>
-                                    <li> <a href="#">Wallets</a> </li>
-                                </ul>
-                            </tr>
-                        </table>
-
-                    </Link>
-                </NavItem>
-
-                <NavItem>
-                    <Link to="/service" className="nav-link"><span className="btn">Our Service</span></Link>
-                </NavItem>
-                <NavItem>
-                    <Link to="/contact" className="nav-link"><span className="btn">Contact Us</span></Link>
-                </NavItem>
-                <NavItem>
-                    <span className="fas fa-search"></span>
-                </NavItem>
-                
-          </Nav>
-        </Collapse>
-      </Navbar>
-
-      {/* <Switch>
-            <Route path="/" exact>
-                <Home />
-            </Route>
-            <Route path="/about">
-                <About />
-            </Route>
-            <Route path="/service">
-                <Service />
-            </Route>
-            <Route path="/contact">
-                <Contact />
-            </Route>
-        </Switch> */}
-    </Router>
-  );
+            <Form style={{ padding: '0 60px'}} 
+                className={(this.state.formSearch === false) ? 'closeFormSearch form-search' : 'form-search'}>
+                <Row>
+                    <Col  md="1" lg="1" >
+                        <i className="fas fa-search"></i>
+                    </Col>
+                    <Col md="10" lg="10" >
+                        <input type="text" name="search" placeholder="Search"/>
+                    </Col>
+                    <Col md="1" lg="1" >
+                        <Button variant="" onClick={this.closeFormSearch}>
+                            <i className="fas fa-times"></i>
+                        </Button>
+                    </Col>
+                </Row>
+            </Form>
+        </div>
+        )
+    }
 }
-
 
 
 export default Header;
